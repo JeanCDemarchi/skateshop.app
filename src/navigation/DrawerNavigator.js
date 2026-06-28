6a9be3116b78a6b492e41b9a0e8fd616b7f580ae
JeanCDemarchi <jenaosei1@gmail.com>
2026-06-28 15:05:06 -0300

    Update DrawerNavigator.js

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -1,36 +1,18 @@
-import {
-  createDrawerNavigator,
-  DrawerContentScrollView,
-  DrawerItem,
-  DrawerItemList,
-} from '@react-navigation/drawer';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
+import AboutScreen from '../screens/AboutScreen';
 import CartScreen from '../screens/CartScreen';
+import ContactScreen from '../screens/ContactScreen';
 import HomeScreen from '../screens/HomeScreen';
+import NewsScreen from '../screens/NewsScreen';
 import OrdersScreen from '../screens/OrdersScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 
 const Drawer = createDrawerNavigator();
 
-function CustomDrawerContent(props) {
-  const handleLogout = () =>
-    props.navigation.getParent()?.reset({
-      index: 0,
-      routes: [{ name: 'Login' }],
-    });
-
-  return (
-    <DrawerContentScrollView {...props}>
-      <DrawerItemList {...props} />
-      <DrawerItem label="Sair" labelStyle={{ color: '#aaa' }} onPress={handleLogout} />
-    </DrawerContentScrollView>
-  );
-}
-
 export default function DrawerNavigator() {
   return (
     <Drawer.Navigator
-      drawerContent={(props) => <CustomDrawerContent {...props} />}
       screenOptions={{
         headerShown: false,
         drawerStyle: {
@@ -44,6 +26,9 @@ export default function DrawerNavigator() {
       <Drawer.Screen name="Pedidos" component={OrdersScreen} />
       <Drawer.Screen name="Carrinho" component={CartScreen} />
       <Drawer.Screen name="Perfil" component={ProfileScreen} />
+      <Drawer.Screen name="Novidades" component={NewsScreen} />
+      <Drawer.Screen name="Sobre" component={AboutScreen} />
+      <Drawer.Screen name="Contato" component={ContactScreen} />
 
       
     </Drawer.Navigator>
