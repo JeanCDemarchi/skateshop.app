9e76047db12948d441c07ab5734f6e3fe98f5564
JeanCDemarchi <jenaosei1@gmail.com>
2026-06-28 14:59:32 -0300

    Update AddProductScreen.js

--- src/screens/AddProductScreen.js
+++ src/screens/AddProductScreen.js
@@ -1,160 +1,183 @@
 import { Ionicons } from '@expo/vector-icons';
 import { useState } from 'react';
-import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Alert, FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 
-export default function AddProductScreen({ route, navigation }) {
-  const { adicionarProduto } = route.params;
 
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  const [price, setPrice] = useState('');
-  const [stock, setStock] = useState('');
-  const [code, setCode] = useState('');
-
-  function salvarProduto() {
-    const novoProduto = {
-      id: Date.now().toString(),
-      name,
-      description,
-      price,
-      stock,
-      code,
+export default function AdminHomeScreen({ navigation }) {
+  const [products, setProducts] = useState([
+    {
+      id: '1',
+      name: 'Chave em T',
+      price: '39,90',
+      stock: 18,
       image: require('../assets/images/tool.png'),
-    };
+    },
+    {
+      id: '2',
+      name: 'Truck Gold',
+      price: '499,90',
+      stock: 7,
+      image: require('../assets/images/truck.png'),
+    },
+    {
+      id: '#98761',
+      name: 'Lixa Emborrachada',
+      price: '99,90',
+      stock: 42,
+      image: require('../assets/images/lixa.png'),
+    },
+    {
+      id: '#18958',
+      name: 'Parafuso Bolts',
+      price: '24,90',
+      stock: 63,
+      image: require('../assets/images/bolts.png'),
+    },
+    {
+      id: '#88654',
+      name: 'Amortecedor PU',
+      price: '49,90',
+      stock: 25,
+      image: require('../assets/images/amortecedor.png'),
+    },
+    {
+      id: '#98898',
+      name: 'Kit Parafusos',
+      price: '39,90',
+      stock: 31,
+      image: require('../assets/images/paraf.png'),
+    },
+    {
+      id: '#45632',
+      name: 'Shape',
+      price: '359,90',
+      stock: 12,
+      image: require('../assets/images/shape.png'),
+    },
+  ]);
 
-    adicionarProduto(novoProduto);
-
-    Alert.alert('Sucesso', 'Produto adicionado com sucesso!');
-    navigation.goBack();
+  function adicionarProduto(novoProduto) {
+    setProducts((listaAtual) => [...listaAtual, novoProduto]);
   }
+
+  function atualizarProduto(produtoEditado) {
+    setProducts((listaAtual) =>
+        listaAtual.map((item) =>
+        item.id === produtoEditado.id ? produtoEditado : item
+        )
+    );
+  }
+
+  function excluirProduto(id) {
+    Alert.alert(
+      'Excluir produto',
+      'Tem certeza que deseja excluir este produto?',
+      [
+        { text: 'Cancelar', style: 'cancel' },
+        {
+          text: 'Excluir',
+          style: 'destructive',
+          onPress: () => {
+            setProducts(products.filter((item) => item.id !== id));
+          },
+        },
+      ]
+    );
+  }
+
+  function editarProduto(produto) {
+  navigation.navigate('EditarProduto', {
+    produto,
+    atualizarProduto,
+  });
+}
 
   return (
     <View style={styles.container}>
-      <View style={styles.content}>
-        <Text style={styles.title}>Nome e descrição</Text>
+      <FlatList
+        data={products}
+        contentContainerStyle={{ paddingBottom: 130 }}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Image source={item.image} style={styles.image} />
 
-        <TextInput
-          placeholder="Nome"
-          value={name}
-          onChangeText={setName}
-          style={styles.input}
-        />
+            <View style={styles.info}>
+              <Text>{item.name}</Text>
+              <Text>{item.code}</Text>
+              <Text>{item.stock} unidades</Text>
+              <Text>R$ {item.price}</Text>
+            </View>
 
-        <TextInput
-          placeholder="Descrição"
-          value={description}
-          onChangeText={setDescription}
-          style={styles.description}
-          multiline
-        />
+            <View style={styles.actions}>
+              <TouchableOpacity onPress={() => excluirProduto(item.id)}>
+                <Ionicons name="trash-outline" size={24} color="#000" />
+              </TouchableOpacity>
 
-        <Text style={styles.label}>Fotos</Text>
+              <TouchableOpacity onPress={() => editarProduto(item)}>
+                <Ionicons name="pencil-outline" size={24} color="#000" />
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+      />
+        <TouchableOpacity
+        style={styles.addButton}
+        onPress={() =>
+            navigation.navigate('AdicionarProduto', {
+            adicionarProduto,
+            })
+        }
+        >
+        <Ionicons name="add" size={38} color="#fff" />
+      </TouchableOpacity>
+      
 
-        <TouchableOpacity style={styles.photoBox}>
-          <Ionicons name="add-circle-outline" size={22} color="#008cff" />
-          <Text style={styles.photoText}>Selecione fotos do produto</Text>
-        </TouchableOpacity>
-
-        <Text style={styles.label}>Preço</Text>
-        <TextInput
-          placeholder="R$ 0,00"
-          value={price}
-          onChangeText={setPrice}
-          style={styles.input}
-        />
-
-        <Text style={styles.label}>Estoque</Text>
-        <TextInput
-          placeholder="0"
-          value={stock}
-          onChangeText={setStock}
-          keyboardType="numeric"
-          style={styles.input}
-        />
-
-        <Text style={styles.label}>Códigos</Text>
-        <TextInput
-          placeholder="SKU"
-          value={code}
-          onChangeText={setCode}
-          style={styles.input}
-        />
-
-        <TouchableOpacity style={styles.button} onPress={salvarProduto}>
-          <Text style={styles.buttonText}>Adicionar produto</Text>
-        </TouchableOpacity>
-      </View>
-
-     
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#fff' },
-
-  content: {
+  container: {
     flex: 1,
-    paddingHorizontal: 45,
-    paddingTop: 25,
+    backgroundColor: '#fff',
   },
 
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 12,
-  },
-
-  label: {
-    fontSize: 12,
-    marginBottom: 5,
-    marginTop: 8,
-  },
-
-  input: {
-    backgroundColor: '#d9d9d9',
-    height: 35,
-    paddingHorizontal: 10,
-    marginBottom: 8,
-  },
-
-  description: {
-    backgroundColor: '#d9d9d9',
-    height: 85,
-    paddingHorizontal: 10,
-    paddingTop: 8,
-    marginBottom: 8,
-    textAlignVertical: 'top',
-  },
-
-  photoBox: {
-    height: 70,
-    borderWidth: 2,
-    borderStyle: 'dashed',
-    borderColor: '#008cff',
-    justifyContent: 'center',
+  card: {
+    flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 10,
+    padding: 20,
+    borderBottomWidth: 1,
+    borderColor: '#000',
   },
 
-  photoText: {
-    color: '#008cff',
-    fontSize: 12,
-    marginTop: 4,
+  image: {
+    width: 90,
+    height: 90,
+    resizeMode: 'contain',
+    marginRight: 15,
   },
 
-  button: {
+  info: {
+    flex: 1,
+  },
+
+  actions: {
+    gap: 18,
+    alignItems: 'center',
+  },
+  addButton: {
+    position: 'absolute',
+    bottom: 30,
+    alignSelf: 'center',
+    width: 75,
+    height: 75,
+    borderRadius: 38,
     backgroundColor: '#000',
-    height: 45,
-    borderRadius: 8,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 25,
-  },
-
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
+    elevation: 10,
   },
 });
